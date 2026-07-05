ae31bdc0b4575d8047bad646a2485fc0a4d86b9c
Hassan552829 <ht3632636@gmail.com>
2026-07-06 00:09:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: ['https://kuwait-saloons.netlify.app', 'https://*.netlify.app', 'http://localhost:3000'],
-  credentials: true
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
